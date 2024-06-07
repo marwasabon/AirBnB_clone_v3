122e8a1e1808250b7b3d797d4ff4cba8ee969674
marwasabon <marwasabon@gmail.com>
2024-06-07 08:55:26 +0000

    task 2 Tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,1 +1,94 @@
+import unittest
+import inspect
+import models
+from models import storage
+from models.user import User
+from models.engine.db_storage import DBStorage
+from models.state import State
+import pycodestyle
+from models.base_model import BaseModel, Base
+import os
+import MySQLdb
+from os import getenv
+
+# set the env variables
+storage_t = os.getenv("HBNB_TYPE_STORAGE")
+class TestDBStorage(unittest.TestCase):
+    """Test documentation and style."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the test environment"""        
+        if type(models.storage) == DBStorage:
+            db = MySQLdb.connect(user=os.getenv("HBNB_MYSQL_USER"),
+                                 passwd=os.getenv("HBNB_MYSQL_PWD"),
+                                 db=os.getenv("HBNB_MYSQL_DB"))
+            cls.cursor = db.cursor()
+            cls.storage = DBStorage()
+            cls.storage.reload()
+            cls.state = State(name="Marwa")
+            cls.user = User(email="marwa@mydomain.com",
+                            password="password")
+            cls.storage._DBStorage__session.add(cls.state)
+            cls.storage._DBStorage__session.add(cls.user)
+            cls.storage._DBStorage__session.commit()
+    
+    def new(self, obj):
+        """Add the object to the current database session."""
+        self.storage.new(obj)
+    
+    @classmethod
+    def tearDown(self):
+        if type(models.storage) == DBStorage:
+            cls.cursor.close()
+            cls.storage._DBStorage__session.close()
+
+    def dtest_all(self):
+        """Test the all() method"""
+        obj_dict = self.storage.all()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertGreaterEqual(len(obj_dict), 0) 
+
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_new(self):
+        """Testing new method."""
+        ny = State(name="New York")
+        self.storage.new(ny)
+        self.assertIn(ny, list(self.storage._DBStorage__session.new))
+        self.storage._DBStorage__session.rollback()
+    
+
+    @unittest.skipIf(models.storage_t != "db", "not testing file storage")
+    def test_get(self):
+        """Testing get method."""
+        self.assertEqual(self.storage.get("User", self.user.id), self.user)
+ 
+    def dtest_get(self):
+        state = State(name="New York4")
+        self.storage.new(state)
+        self.storage.save()
+        
+        result = self.storage.all(State).values()
+        self.assertEqual(state, list(result)[0])
+
+    def dtest_count(self):
+        state1 = State(name="New York")
+        state2 = State(name="New York2")
+        self.storage.new(state1)
+        self.storage.new(state2)
+        self.storage.save()
+
+        count = self.storage.count(State)
+        self.assertEqual(count, 2)
+
+    def dtest_pep8_conformance(self):
+        """ this is test pycodestyle"""
+        style = pycodestyle.StyleGuide()
+        result = style.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,138 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
+from models.state import State
+from models.city import City
+import os
+from models.engine import file_storage
+import pep8
+
+FileStorage = file_storage.FileStorage
+
+
+class test_fileStorage(unittest.TestCase):
+    """ Class to test the file storage method """
+
+    def setUp(self):
+        """ Set up test environment """
+        del_list = []
+        for key in storage._FileStorage__objects.keys():
+            del_list.append(key)
+        for key in del_list:
+            del storage._FileStorage__objects[key]
+
+    def tearDown(self):
+        """ Remove storage file at end of tests """
+        try:
+            os.remove('file.json')
+        except:
+            pass
+
+    def test_obj_list_empty(self):
+        """ __objects is initially empty """
+        self.assertEqual(len(storage.all()), 0)
+
+    def test_new(self):
+        """ New object is correctly added to __objects """
+        new = BaseModel()
+        temp = None
+        obj = None
+        for obj in storage.all().values():
+            temp = obj
+        self.assertTrue(temp is obj)
+
+    def test_all(self):
+        """ __objects is properly returned """
+        temp = None
+        new = BaseModel()
+        temp = storage.all()
+        self.assertIsInstance(temp, dict)
+    def test_base_model_instantiation(self):
+        """ File is not created on BaseModel save """
+        new = BaseModel()
+        self.assertFalse(os.path.exists('file.json'))
+
+    def test_empty(self):
+        """ Data is saved to file """
+        new = BaseModel()
+        thing = new.to_dict()
+        new.save()
+        new2 = BaseModel(**thing)
+        self.assertNotEqual(os.path.getsize('file.json'), 0)
+
+    def test_save(self):
+        """ FileStorage save method """
+        new = BaseModel()
+        storage.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def dtest_reload(self):
+        """ Storage file is successfully loaded to __objects """
+        new = BaseModel()
+        storage.save()
+        storage.reload()
+        for obj in storage.all().values():
+            loaded = obj
+        self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
+
+    def dtest_reload_empty(self):
+        """ Load from an empty file """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+    def test_reload_from_nonexistent(self):
+        """ Nothing happens if file does not exist """
+        self.assertEqual(storage.reload(), None)
+
+    def test_base_model_save(self):
+        """ BaseModel save method calls storage save """
+        new = BaseModel()
+        new.save()
+        self.assertTrue(os.path.exists('file.json'))
+
+    def test_type_path(self):
+        """ Confirm __file_path is string """
+        self.assertEqual(type(storage._FileStorage__file_path), str)
+
+    def test_type_objects(self):
+        """ Confirm __objects is a dict """
+        self.assertEqual(type(storage.all()), dict)
+
+    def dtest_key_format(self):
+        """ Key is properly formatted """
+        new = BaseModel()
+        _id = new.to_dict()['id']
+        for key in storage.all().keys():
+            temp = key
+        self.assertEqual(temp, 'BaseModel' + '.' + _id)
+
+    def test_storage_var_created(self):
+        """ FileStorage object storage created """
+        from models.engine.file_storage import FileStorage
+        print(type(storage))
+        self.assertEqual(type(storage), FileStorage)
+
+    @unittest.skipIf(type(storage) != FileStorage, "Testing DBStorage")
+    def test_count_specified_class(self):
+        """Test count Function"""
+        storage = FileStorage()
+        FileStorage._FileStorage__objects = {}
+        ca = State(name="California")
+        key = ca.__class__.__name__
+        FileStorage._FileStorage__objects["{}.{}".format(key, ca.id)] = ca
+        sf = City(name="San Francisco")
+        key = sf.__class__.__name__
+        FileStorage._FileStorage__objects["{}.{}".format(key, sf.id)] = sf
+        self.assertEqual(storage.count("State"), 1)
+
+    def test_pep8_conformance_file_storage(self):
+        """Test that models/engine/file_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warnings).")
 
