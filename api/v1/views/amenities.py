44f780fe823d04bfc3dae93437953c1818fa1f77
marwasabon <marwasabon@gmail.com>
2024-06-09 03:05:42 +0000

    task 8 amentitites

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" Python script for State Routes & views"""
+from api.v1.views import app_views
+from models.state import State
+from models import storage
+from flask import jsonify, abort, request
+from models.amenity import Amenity
+
+
+@app_views.route("/amenitites", methods=["GET", "POST"])
+def get_amenites():
+    """ Get methods for amenitites routes"""
+    # get method
+    if request.method == "GET":
+        amenitites = []
+        for amenity in storage.all("Amenity").values():
+            amenitites.append(amenity.to_dict())
+        return jsonify(amenitites)
+
+    # post method
+    data = request.get_json(silent=True)
+    if not data:
+        return "Not a JSON", 400
+    if "name" not in data:
+        return "Missing name", 400
+
+    # create new amenity
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route("/amenitites/<amenity_id>", methods=["GET", "DELETE", "PUT"])
+def get_amenity_id(amenity_id):
+    """ Get methods for retriving amenitites objects by id"""
+    amenity = storage.get("Amenity", amenity_id)
+    if not amenity:
+        abort(404)
+
+    # DELETE method
+    if request.method == "DELETE":
+        amenity.delete()
+        storage.save()
+        return jsonify({}), 200
+
+    # GET method
+    elif request.method == "GET":
+        return jsonify(amenity.to_dict())
+
+    # PUT method
+    data = request.get_json(silent=True)
+    if not data:
+        return "Not a JSON", 400
+    keys_ignored = {"id", "created_at", "updated_at"}
+    [setattr(amenity, k, v) for k, v in data.items() if k not in keys_ignored]
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
