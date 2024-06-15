f4e2d2f18e0259b0fb8f51530bbb6d62d09d6ce0
marwasabon <marwasabon@gmail.com>
2024-06-15 23:17:56 +0000

    afix places & reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -3,42 +3,53 @@
 from api.v1.views import app_views
 from models import storage
 from flask import jsonify, abort, request
-from models.Amenity import Amenity
+from models.review import Review
 
 
-@app_views.route("/amenities", methods=["GET", "POST"])
-def amenities():
-    """ Get methods for amenitites routes"""
+@app_views.route("/places/<place_id>/reviews", methods=["GET", "POST"])
+def reviews(place_id):
+    """Defines the GET and POST method for reviews on /places route.
+    """
+    place = storage.get("Place", place_id)
+    if place is None:
+        abort(404)
+
     # GET method
     if request.method == "GET":
-        return jsonify([a.to_dict() for a in storage.all("Amenity").values()])
+        return jsonify([r.to_dict() for r in place.reviews])
 
     # POST method
     data = request.get_json(silent=True)
     if data is None:
         return "Not a JSON", 400
-    if data.get("name") is None:
-        return "Missing name", 400
-    amenity = Amenity(**data)
-    amenity.save()
-    return jsonify(amenity.to_dict()), 201
+    user_id = data.get("user_id")
+    if user_id is None:
+        return "Missing user_id", 400
+    if storage.get("User", user_id) is None:
+        abort(404)
+    if data.get("text") is None:
+        return "Missing text", 400
+    data["place_id"] = place_id
+    review = Review(**data)
+    review.save()
+    return jsonify(review.to_dict()), 201
 
 
-@app_views.route("/amenities/<amenity_id>", methods=["GET", "DELETE", "PUT"])
-def amenity_id(amenity_id):
-    """Defines GET, PUT and DELETE methods for a specific ID on /amenities.
+@app_views.route("/reviews/<review_id>", methods=["GET", "DELETE", "PUT"])
+def review_id(review_id):
+    """Defines the GET, PUT and DELETE methods for a specific ID on reviews.
     """
-    amenity = storage.get("Amenity", amenity_id)
-    if amenity is None:
+    review = storage.get("Review", review_id)
+    if review is None:
         abort(404)
 
-    # GET method defin
+    # GET method
     if request.method == "GET":
-        return jsonify(amenity.to_dict())
+        return jsonify(review.to_dict())
 
-    # DELETE method defin
+    # DELETE method
     elif request.method == "DELETE":
-        storage.delete(amenity)
+        storage.delete(review)
         storage.save()
         return jsonify({})
 
@@ -46,7 +57,7 @@ def amenity_id(amenity_id):
     data = request.get_json(silent=True)
     if data is None:
         return "Not a JSON", 400
-    avoid = {"id", "created_at", "updated_at"}
-    [setattr(amenity, k, v) for k, v in data.items() if k not in avoid]
-    amenity.save()
-    return jsonify(amenity.to_dict())
+    avoid = {"id", "user_id", "place_id", "created_at", "updated_at"}
+    [setattr(review, k, v) for k, v in data.items() if k not in avoid]
+    review.save()
+    return jsonify(review.to_dict())
