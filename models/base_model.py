4df9af401dfcd5c8693c57616b44bfaa16746a7e
marwasabon <marwasabon@gmail.com>
2024-06-15 22:53:01 +0000

    afix user 3

--- models/base_model.py
+++ models/base_model.py
@@ -68,7 +68,6 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        return new_dict
         if save_fs is None:
             if "password" in new_dict:
                 del new_dict["password"]
