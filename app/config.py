8cf028cc529ff58e0f470da40e06b4839e05e959
marwasabon <marwasabon@gmail.com>
2024-07-12 04:32:44 +0000

    congig

--- app/config.py
+++ app/config.py
@@ -1,7 +1,12 @@
 # /app/config.py
+import os
+import secrets
+
 
 class Config:
-    SECRET_KEY = 'e8be6c76f90f01893eedc58ee07c65fcc1e4339b1a854dc1edc9969402a84bc2'
+    SECRET_KEY = os.environ.get('SECRET_KEY', secrets.token_hex(16))
+    #the below code is bad practice
+    #SECRET_KEY = 'e8be6c76f90f01893eedc58ee07c65fcc1e4339b1a854dc1edc9969402a84bc2'
     SQLALCHEMY_DATABASE_URI = 'mysql://silver:Password_12345@localhost/contact'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = '/images'
