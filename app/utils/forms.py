3f5755686cab618647064a34fecab51d31b12955
marwasabon <marwasabon@gmail.com>
2024-07-18 15:24:12 +0300

    chore: Update forms.py with additional fields and validators

--- app/utils/forms.py
+++ app/utils/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField, SelectField, TextAreaField, FileField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from ..models.user import User
+from wtforms import StringField,PasswordField, SelectField, TextAreaField, FileField , SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email,Length, EqualTo, ValidationError
+from ..models.user import User 
 from flask_login import login_required, current_user
 
 
@@ -31,22 +30,7 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('Please use a different email address.')
-class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Update')
-
-    def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
-
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose a different one.')
+        
 
 class DeleteAccountForm(FlaskForm):
     submit = SubmitField('Delete Account')
--- run.py
+++ run.py
@@ -8,4 +8,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='192.168.66.165',debug=True)
+    app.run(debug=True)
